1d0b319e03ce3a60bcd98e8a6ba27d9afef7e0fc
Laurent Tempier <laurent.tempier@gmail.com>
2017-06-18 12:03:10 -0400

    wip gallery

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,29 @@
         var $postContent = $(".post-content");
         $postContent.fitVids();
 
+        if (_GALLERY) {
+            var $gallery = $('#gallery');
+            $.each(_GALLERY, function (item) {
+
+                var $item = $("<li class='gallery-item'></li>");
+                $item.append($('<img src="' + this.src + '"/>'));
+                //$item.append($('<span>' + this.legend + '</span>'));
+
+                $gallery.append($item);
+            });
+
+            $gallery.imagesLoaded().done(function () {
+                $gallery.isotope({
+                    //percentPosition: true,
+                    itemSelector: '.gallery-item',
+                    //masonry: {
+                    //    columnWidth: '.grid-sizer'
+                    //}
+                });
+            });
+
+        }
+
     });
 
 })(jQuery);
